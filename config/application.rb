b983342959e07d8d5a4114207afa32f32d9ce9f4
Tanner Noble <tannernoble@Tanners-iMac.local>
2019-11-06 23:57:28 -0600

    Created seeds file for sample data

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module DevcampPortfolio
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
